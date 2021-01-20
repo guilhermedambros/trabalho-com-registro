858745e06d66b3af8b8fd56ad2c915cafcf8d0bf
Guilherme <gdambros@emater.tche.br>
2021-01-20 07:18:53 -0300

    Adicionado plugin para visualização d e logs em admin/logs

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'mi
     Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
     Route::resource('users', 'UsersController');
 
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+
 });
 Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function () {
 // Change password
