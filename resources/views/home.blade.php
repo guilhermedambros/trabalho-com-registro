72384eaa605433c9278441ada29a3cb6d6823907
Guilherme <gdambros@emater.tche.br>
2021-02-09 06:29:10 -0300

    Adicionado atalho na home para demandas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,6 +39,29 @@
                 </div>
             </div>
             @endcan
+            @can('demanda_acessar')
+            <!-- light mode -->
+            <div class="card mx-auto sm:px-6 lg:px-8 m-4">
+                <div class="overflow-hidden shadow-md">
+                    <!-- card header -->
+                    <div class="px-6 py-4 bg-white border-b border-gray-200 font-bold uppercase">
+                        Cadastro de Demandas
+                    </div>
+
+                    <!-- card body -->
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        Esse cadastro permite manipular informações referentes a demandas, além de permitir o registro de ativididades.
+                    </div>
+
+                    <!-- card footer -->
+                    <div class="p-6 bg-white border-gray-200 text-right">
+                        <!-- button link -->
+                        <a class="bg-blue-500 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-blue-400 rounded uppercase" 
+                            href="{{ route('demandas.index') }}">Acessar</a>
+                    </div>
+                </div>
+            </div>
+            @endcan
             
         
         </div>
